7127935a6c250cfa750c56277f5ddfefd3f0bc8c
hussynnawaz <hussynnawaz>
2022-11-22 20:38:48 +0500

    Connection Query DB Connection

---
+++ connectionquery.js
@@ -0,0 +1,21 @@
+//connect to mysql database nodejs?
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'example.org',
+  user     : 'bob',
+  password : 'secret',
+});
+
+connection.connect(function(err) {
+  // connected! (unless `err` is set)
+});
+
+
+var post  = {id: 1, title: 'Hello MySQL'};
+var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
+  // Neat!
+});
+console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
+
+
